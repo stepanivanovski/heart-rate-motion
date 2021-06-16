ab7e7dbcb141894534ff4cf70c01f3da10408e39
Stepan <usb-1999@mail.ru>
2021-06-16 12:52:22 +0600

    tiny-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-    nav:true,
-    navElement:"<button><img src = '../img/slider/button_next.png'></img></button>",
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
-  });
+// "use strict";
+//import { tns } from "../../node_modules/tiny-slider/src/tiny-slider";
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  controls: false,
+  controlsText: ["<img src = '../img/slider/button_prev.png'", "<img src = '../img/slider/button_next.png'"],
+  navPosition: 'bottom'
+});
+
+document.querySelector('.next-btn').addEventListener('click', function () {
+  slider.goTo("next");
+});
+
+document.querySelector('.prev-btn').addEventListener('click', function () {
+  slider.goTo("prev");
 });
